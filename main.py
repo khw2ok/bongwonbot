44909a011893569e2313df9c384663fe5382391c
nickid <92078139+khw2ok@users.noreply.github.com>
2022-06-13 06:34:22 +0900

    Rename app.py to main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from comcigan import School
 from bs4 import BeautifulSoup
 from datetime import datetime
-from flask import jsonify, Flask, request
+from flask import jsonify, Flask, redirect, request
 
 import dotenv
 import json
 import os
+import random
 import re
-from pip import main
 import requests
 
 app = Flask(__name__)
@@ -18,15 +18,19 @@ school = School("봉원중학교")
 def index():
     return "Hello World!"
 
-@app.route("/test", methods=["POST"])
-def test():
+@app.route("/api/")
+def api():
+    return redirect("/")
+
+@app.route("/api/test", methods=["POST"])
+def api_test():
     res = {
         "version": "2.0",
         "template": {
             "outputs": [
                 {
                     "simpleText": {
-                        "text": "answer"
+                        "text": "안녕하세요!"
                     }
                 }
             ]
@@ -35,16 +39,15 @@ def test():
 
     return jsonify(res)
 
-@app.route("/food", methods=["POST"])
-def food():
+@app.route("/api/meal", methods=["POST"])
+def api_meal():
     req = request.get_json()
 
     bot_plugin_date : str = req["action"]["detailParams"]["bot_plugin_date"]["value"]
     bot_date : str = f"{bot_plugin_date[33]}{bot_plugin_date[34]}{bot_plugin_date[35]}{bot_plugin_date[36]}{bot_plugin_date[37]}{bot_plugin_date[38]}{bot_plugin_date[39]}{bot_plugin_date[40]}{bot_plugin_date[41]}{bot_plugin_date[42]}"
 
-    url = requests.get("https://schoolmenukr.ml/api/middle/B100001561")
-    file = url.text
-    data = json.loads(file)
+    res = requests.get("https://schoolmenukr.ml/api/middle/B100001561").text
+    data = json.loads(res)
     #print(re.sub("-?\d+|\'|\?|\'|\.|", "", str(data)))
 
     date = datetime.strptime(str(bot_date), "%Y-%m-%d")
@@ -57,36 +60,63 @@ def food():
     answer_title = f"{str(now_month)}월 {date.day}일 {days[datetime(date.year, datetime.now().month, date.day).weekday()]}요일"
     answer_desc : str = re.sub("-?\d+|\'|\.|\'|\#|\'|\[|\'|\]", "", str(date_food))
     answer_image = "https://raw.githubusercontent.com/bongwonbot/bongwonbot/main/img/spoon.png"
-    print(date.month)
-    print(now_month)
+
     if date.month != str(now_month):
         answer_image = "https://raw.githubusercontent.com/bongwonbot/bongwonbot/main/img/warning.png"
-    if answer_desc == "":
+    elif answer_desc == "":
         answer_image = "https://raw.githubusercontent.com/bongwonbot/bongwonbot/main/img/warning.png"
         answer_desc = "급식 정보가 없습니다."
+    else:
+        answer_image = "https://raw.githubusercontent.com/bongwonbot/bongwonbot/main/img/spoon.png"
+
 
     res = {
         "version": "2.0",
         "template": {
             "outputs": [
                 {
-                    "basicCard": {
-                        "title": answer_title,
-                        "description": answer_desc,
-                        "thumbnail": {
-                            "imageUrl": answer_image,
-                            "fixedRatio": True
+                    "itemCard": {
+                        "head": {
+                            "title": "급식"
+                        },
+                        "itemList": [
+                            {
+                                # "title": "날짜",
+                                # "description": answer_title
+                            }
+                        ],
+                        "itemListSummary": {
+                            "title": "",
+                            "description": answer_desc
                         }
                     }
                 }
             ]
         }
     }
+
+    # res = {
+    #     "version": "2.0",
+    #     "template": {
+    #         "outputs": [
+    #             {
+    #                 "basicCard": {
+    #                     "title": answer_title,
+    #                     "description": answer_desc,
+    #                     "thumbnail": {
+    #                         "imageUrl": answer_image,
+    #                         "fixedRatio": True
+    #                     }
+    #                 }
+    #             }
+    #         ]
+    #     }
+    # }
     return jsonify(res)
 
-@app.route("/weather", methods=["POST"])
-def weather():
-    apikey : str = os.environ["APIKEY"]
+@app.route("/api/weather", methods=["POST"])
+def api_weather():
+    apikey : str = os.environ["WEATHER_APIKEY"]
     api = f"http://api.openweathermap.org/data/2.5/weather?appid={apikey}&lang=kr&q=Seoul,KR&"
 
     file = api.format(key=apikey)
@@ -146,8 +176,8 @@ def weather():
 
     return jsonify(res)
 
-@app.route("/timetable", methods=["POST"])
-def timetable():
+@app.route("/api/timetable", methods=["POST"])
+def api_timetable():
     req = request.get_json()
 
     bot_school_grade = req["action"]["detailParams"]["bot_school_grade"]["value"]
@@ -189,7 +219,7 @@ def timetable():
                                 "title": f"{bot_date_week} 시간표"
                             },
                             "thumbnail": {
-                                "imageUrl": "https://raw.githubusercontent.com/bongwonbot/bongwonbot/main/img/sun_behind_rain_cloud.png",
+                                "imageUrl": "https://raw.githubusercontent.com/bongwonbot/bongwonbot/main/img/calendar.png",
                                 "fixedRatio": True
                             },
                             "itemList": [
@@ -229,7 +259,6 @@ def timetable():
                 ]
             }
         }
-
     except IndexError:
         try:
             res = {
@@ -246,7 +275,7 @@ def timetable():
                                     "title": f"{bot_date_week} 시간표"
                                 },
                                 "thumbnail": {
-                                    "imageUrl": "https://raw.githubusercontent.com/bongwonbot/bongwonbot/main/img/sun_behind_rain_cloud.png",
+                                    "imageUrl": "https://raw.githubusercontent.com/bongwonbot/bongwonbot/main/img/calendar.png",
                                     "fixedRatio": True
                                 },
                                 "itemList": [
@@ -283,57 +312,159 @@ def timetable():
                 }
             }
         except IndexError:
-            res = {
-                "version": "2.0",
-                "template": {
-                    "outputs": [
-                        {
-                            "itemCard": {
-                                "imageTitle": {
-                                    "title": f"{req_school_grade}학년 {req_school_class}반",
-                                    "description": f"봉원중학교의 시간표"
-                                },
-                                "head": {
-                                    "title": f"{bot_date_week} 시간표"
-                                },
-                                "thumbnail": {
-                                    "imageUrl": "https://raw.githubusercontent.com/bongwonbot/bongwonbot/main/img/sun_behind_rain_cloud.png",
-                                    "fixedRatio": True
-                                },
-                                "itemList": [
-                                    {
-                                        "title": "1교시",
-                                        "description": f"{answer[0][0]} ({answer[0][2]})"
+            try:
+                res = {
+                    "version": "2.0",
+                    "template": {
+                        "outputs": [
+                            {
+                                "itemCard": {
+                                    "imageTitle": {
+                                        "title": f"{req_school_grade}학년 {req_school_class}반",
+                                        "description": f"봉원중학교의 시간표"
                                     },
-                                    {
-                                        "title": "2교시",
-                                        "description": f"{answer[1][0]} ({answer[1][2]})"
+                                    "head": {
+                                        "title": f"{bot_date_week} 시간표"
                                     },
-                                    {
-                                        "title": "3교시",
-                                        "description": f"{answer[2][0]} ({answer[2][2]})"
+                                    "thumbnail": {
+                                        "imageUrl": "https://raw.githubusercontent.com/bongwonbot/bongwonbot/main/img/calendar.png",
+                                        "fixedRatio": True
                                     },
-                                    {
-                                        "title": "4교시",
-                                        "description": f"{answer[3][0]} ({answer[3][2]})"
-                                    },
-                                    {
-                                        "title": "5교시",
-                                        "description": f"{answer[4][0]} ({answer[4][2]})"
-                                    }
-                                ],
-                                "itemListAlignment" : "left",
-                                "buttonLayout" : "vertical"
+                                    "itemList": [
+                                        {
+                                            "title": "1교시",
+                                            "description": f"{answer[0][0]} ({answer[0][2]})"
+                                        },
+                                        {
+                                            "title": "2교시",
+                                            "description": f"{answer[1][0]} ({answer[1][2]})"
+                                        },
+                                        {
+                                            "title": "3교시",
+                                            "description": f"{answer[2][0]} ({answer[2][2]})"
+                                        },
+                                        {
+                                            "title": "4교시",
+                                            "description": f"{answer[3][0]} ({answer[3][2]})"
+                                        },
+                                        {
+                                            "title": "5교시",
+                                            "description": f"{answer[4][0]} ({answer[4][2]})"
+                                        }
+                                    ],
+                                    "itemListAlignment" : "left",
+                                    "buttonLayout" : "vertical"
+                                }
                             }
-                        }
-                    ]
+                        ]
+                    }
                 }
-            }
+            except:
+                res = {
+                    "version": "2.0",
+                    "template": {
+                        "outputs": [
+                            {
+                                "itemCard": {
+                                    "imageTitle": {
+                                        "title": f"{req_school_grade}학년 {req_school_class}반",
+                                        "description": f"봉원중학교의 시간표"
+                                    },
+                                    "head": {
+                                        "title": f"{bot_date_week} 시간표"
+                                    },
+                                    "thumbnail": {
+                                        "imageUrl": "https://raw.githubusercontent.com/bongwonbot/bongwonbot/main/img/warning.png",
+                                        "fixedRatio": True
+                                    },
+                                    "itemList": [
+                                        {
+                                            "title": "오류",
+                                            "description": f"{req_school_grade}학년 {req_school_class}반 {bot_date_week} 시간표를 찾을 수 없습니다."
+                                        }
+                                    ],
+                                    "itemListAlignment" : "left",
+                                    "buttonLayout" : "vertical"
+                                }
+                            }
+                        ]
+                    }
+                }
 
     return jsonify(res)
 
-@app.route("/virus", methods=["POST"])
-def virus():
+@app.route("/api/plan")
+def api_plan():
+    # req = request.get_json()
+
+    # bot_plugin_date : str = req["action"]["detailParams"]["bot_plugin_date"]["value"]
+
+    # date = datetime.strptime(str(bot_date), "%Y-%m-%d")
+
+    api_key : str = os.environ["NEIS_APIKEY"]
+    if datetime.now().month < 10:
+        url = f"https://open.neis.go.kr/hub/SchoolSchedule?KEY={api_key}&Type=json&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7132140&AA_FROM_YMD={datetime.now().year}0{datetime.now().month}01&AA_TO_YMD={datetime.now().year}0{datetime.now().month}32"
+    else:
+        url = f"https://open.neis.go.kr/hub/SchoolSchedule?KEY={api_key}&Type=json&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7132140&AA_FROM_YMD={datetime.now().year}{datetime.now().month}01&AA_TO_YM={datetime.now().year}{datetime.now().month}32"
+
+    res = requests.get(url).text
+    data = json.loads(res)
+    #print(re.sub("-?\d+|\'|\?|\'|\.|", "", str(data)))
+
+    print(data["SchoolSchedule"][0]["head"][0]["list_total_count"])
+    print(data["SchoolSchedule"][1]["row"][-1])
+
+    res = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "basicCard": {
+                        "title": "answer_title",
+                        "description": "answer_desc",
+                        "thumbnail": {
+                            "imageUrl": "answer_image",
+                            "fixedRatio": True
+                        }
+                    }
+                }
+            ]
+        }
+    }
+    return jsonify(data["SchoolSchedule"][1]["row"][-1])
+
+@app.route("/api/quotes", methods=["POST"])
+def api_quotes():
+    list = [
+        '"피할수 없으면 즐겨라." - 로버트 엘리엇',
+        '"행복의 문이 하나 닫히면 다른 문이 열린다. 그러나 우리는 종종 닫힌 문을 멍하니 바라보다가 우리를 향해 열린 문을 보지 못하게 된다." - 헬렌 켈러',
+        '"나 자신에 대한 자신감을 잃으면온 세상이 나의 적이 된다." - 랄프 왈도 에머슨',
+        '"항상 맑으면 사막이 된다. 비가 내리고 바람이 불어야만 비옥한 땅이 된다." - 스페인 속담',
+        '"인생은 곱셈이다. 어떤 기회가 와도 내가 제로면 아무런 의미가 없다." - 나카무라 미츠루',
+        '"실패란 넘어지는 것이 아니라, 넘어진 자리에 머무는 것이다." - 아네스 안 「프린세스 라 브라바」 中',
+        '"사람들은 가치보다 가격에 더 주목한다. 하지만 가격은 당신이 지불하는것이고, 가치는 당신이 얻는것이다." - 워렌 버핏',
+        '"거짓말이 달아준 날개로 당신은 얼마든지 멀리 갈 수 있습니다. 그렇지만 다시 돌아오는 길은 어디에도 없어요." - 파울로 코엘료 「마법의 순간」 中',
+        '"언제나 인생은 99.9%의 일상과 0.1%의 낯선 순간이었다. 이제 더 이상 기대되는 일이 없다고 슬퍼하기엔 99.9%의 일상이 너무도 소중했다." - 이미예 「달러구트 꿈 백화점 2」 中',
+        '"때로는 살아있는 것 조차도 용기가 될 때가 있다." - 세네카',
+        '"당신은 아주 짧은 시간에 태도를 바꿀 수 있습니다. 그 짧은 시간에 태도를 바꾸면, 당신은 하루 전체를 바꿀 수 있습니다." - 스펜서 존슨',
+        '"사막이 아름다운 것은, 어디엔가 샘을 숨기고 있기 때문이야." - 앙투안 드 생텍쥐페리  「어린왕자」 中',
+        '"어른들은 누구나 처음에는 어린이였다. 그러나 그것을 기억하는 어른은 별로 없다." - 앙투안 드 생텍쥐페리  「어린왕자」 中',
+        '"바다는 비에 젖지 않는다." - 어니스트 헤밍웨이 「노인과 바다」 中',
+        '"새는 알에서 나오기 위해 투쟁한다. 알은 세계이다. 태어나려는 자는 하나의 세계를 깨뜨려야 한다. 새는 신에게로 날아간다. 그 신의 이름은 아프락사스다." - 헤르만 헤세 「데미안」 中'
+    ]
+
+    res = {
+        "version": "1.0",
+        "name": "quotes",
+        "data": {
+            "quote": random.choice(list)
+        }
+    }
+
+    return jsonify(res)
+
+@app.route("/api/virus", methods=["POST"])
+def api_virus():
     url = "http://ncov.mohw.go.kr/"
     
     res = requests.get(url) #covid_virus
@@ -363,28 +494,14 @@ def virus():
         }
     }
 
-@app.route("/info", methods=["POST"])
-def info():
-    url = "https://www.schoolinfo.go.kr/ei/ss/Pneiss_b01_s0.do?GS_CD=S010001561"
-    
-    res = requests.get(url, verify=False) #covid_virus
-    soup = BeautifulSoup(res.text, "html.parser")
-
-    students_num = soup.select("div.KeyInfo > div.wrap_box > div.box")[2].text #students_num
-    teachers_num = soup.select("div.basicInfo > div.basic_data > span.md")[4].text.split()[2] #students_num
-
-    r_students_num = str(re.sub(r'[^0-9]', "", students_num)) #students_num
-    r_teachers_num = re.sub(r'[^0-9]', "", teachers_num) #students_num
-
-    return {
-        "version": "1.0",
-        "name": "info",
-        "data": {
-            "students": f"{r_students_num}",
-            "teachers": f"{r_teachers_num}"
-        }
-    }
+# return {
+#     "version": "1.0",
+#     "name": "info",
+#     "data": {
+#         "students": f"{r_students_num}",
+#         "teachers": f"{r_teachers_num}"
+#     }
+# }
 
 if __name__ == "__main__":
-    #run(selfcheck())
     app.run(debug=True)
